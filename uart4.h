178d2204e01f7b21f7583f44ae1aa2a3d4536010
Jim <51113579+jim11662418@users.noreply.github.com>
2024-01-15 07:55:20 -0500

    Delete uart4.h

--- uart4.h
+++
@@ -1,6 +0,0 @@
-void uart4_init(void);
-void send_to_printer_board(unsigned int wwCommand);
-void send_to_printer_board_wait(unsigned int wwCommand);
-char printer_board_reply_avail(void);
-unsigned int get_printer_board_reply(void);
-

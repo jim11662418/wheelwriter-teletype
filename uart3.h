091aade0823c9985d721f17356f7d31960fa86b7
Jim <51113579+jim11662418@users.noreply.github.com>
2024-01-15 07:55:40 -0500

    Delete uart3.h

--- uart3.h
+++
@@ -1,6 +0,0 @@
-void uart3_init(void);
-void send_ACK_to_function_board(void);
-void send_to_function_board(unsigned int wwCommand);
-char function_board_cmd_avail(void);
-unsigned int get_function_board_cmd(void);
-
